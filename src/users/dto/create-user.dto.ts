f396726df481d7e97b40d4689ea129a5cdd825c5
Theocrsb <theo_crosbie@yahoo.fr>
2022-12-01 15:46:59 +0100

    creation dto user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,10 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+  lastname: string;
+  firstname: string;
+  age: number;
+  gender: string;
+  weight: number;
+  height: number;
+  email: string;
+  password: string;
+}
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  lastname: string;
+  firstname: string;
+  age: number;
+  gender: string;
+  weight: number;
+  height: number;
+  email: string;
+  password: string;
+}
