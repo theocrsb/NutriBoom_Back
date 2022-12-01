bd7f5f85edca97a1f8c72805a2687fb9a2655d1e
Theocrsb <theo_crosbie@yahoo.fr>
2022-12-01 14:46:24 +0100

    installation dotenv et typeorm pg + configuration appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// ajout typeORM module
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: process.env.MODE === 'DEV' ? true : false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
