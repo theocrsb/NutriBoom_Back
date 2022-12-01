29b55f5850b7a9b7f3abf98ac4867f112e89dd38
Theocrsb <theo_crosbie@yahoo.fr>
2022-12-01 14:40:33 +0100

    configuration main.ts + gitignore

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // ajout terme 'api' après http://localhost:8080/api/
+  app.setGlobalPrefix('api');
+  // changement du port 3000 avec le port 8080
+  await app.listen(8080);
 }
 bootstrap();
