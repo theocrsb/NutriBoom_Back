69dc15ed965dc244b31dc2680d2d5b85a2bc4e47
eric <eric.jalfre@sncf.fr>
2022-12-02 09:45:22 +0100

    extension classes update

--- src/meals/dto/update-meal.dto.ts
+++ src/meals/dto/update-meal.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateMealDto } from './create-meal.dto';
 
-export class UpdateMealDto extends PartialType(CreateMealDto) {}
+export class UpdateMealDto {}
