75ec5f24d792ead3cb0d0d7d3f8ca49ae1d7d372
eric <eric.jalfre@sncf.fr>
2022-12-06 12:41:12 +0100

    fin crud table role

--- src/role/role.service.ts
+++ src/role/role.service.ts
@@ -19,7 +19,7 @@ export class RoleService {
     return await this.roleRepository.find();
   }
 
-  async findOne(id: number): Promise<Role> {
+  async findOne(id: string): Promise<Role> {
     const roleFound = await this.roleRepository.findOneBy({ id: id });
     if (!roleFound) {
       throw new NotFoundException(`Pas de rôles avec l'id: ${id}`);
@@ -27,12 +27,15 @@ export class RoleService {
     return roleFound;
   }
 
-  async update(id: number, updateRoleDto: UpdateRoleDto): Promise<Role> {
+  async update(id: string, updateRoleDto: UpdateRoleDto): Promise<Role> {
     const updateRole = await this.findOne(id);
+    if (updateRole.label !== undefined) {
+      updateRole.label = updateRoleDto.label;
+    }
     return await this.roleRepository.save(updateRole);
   }
 
-  async remove(id: number) {
+  async remove(id: string) {
     const result = await this.roleRepository.delete({ id });
     if (result.affected === 0) {
       throw new NotFoundException(`Pas de rôles avec l'id: ${id}`);
