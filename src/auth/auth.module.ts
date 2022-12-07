aec5c8cc107d0bd5549d02f204adfeefa2c3346c
Theocrsb <theo_crosbie@yahoo.fr>
2022-12-07 10:08:40 +0100

    debut auth module et service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
+import * as dotenv from 'dotenv';
 
+dotenv.config({ path: '.env' });
 @Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.SECRET_KEY,
+      signOptions: { expiresIn: '4h' },
+    }),
+  ],
   providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
