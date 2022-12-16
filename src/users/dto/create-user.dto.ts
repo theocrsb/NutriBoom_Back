36fc0562675e04c58f5b3e113e8191a13de2763a
Theocrsb <theo_crosbie@yahoo.fr>
2022-12-16 17:33:08 +0100

    ajout restriction entre formulaire email + password

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,15 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 import { Activity } from 'src/activity/entities/activity.entity';
 import { Exercices } from 'src/exercices/entities/exercice.entity';
 import { Role } from 'src/role/entities/role.entity';
-
+//ajout des decorateurs depuis 'class-validator' pour gerer les formulaire
 export class CreateUserDto {
   lastname: string;
   firstname: string;
@@ -10,7 +17,17 @@ export class CreateUserDto {
   gender: string;
   weight: number;
   height: number;
+  @IsEmail()
+  @IsString()
+  @MinLength(4)
   email: string;
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'password must have 1 lower case letter, 1 upper case letter and 1 number or special character',
+  })
   password: string;
   ratio: number;
   role?: Role;
