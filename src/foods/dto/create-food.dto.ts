e5da5924eed9101c9876bbdbf686426a78a609d2
Theocrsb <theo_crosbie@yahoo.fr>
2023-01-17 09:41:05 +0100

    MAJ DTO pour create

--- src/foods/dto/create-food.dto.ts
+++ src/foods/dto/create-food.dto.ts
@@ -1,15 +1,16 @@
 import { IsBoolean, IsNumber, IsString } from 'class-validator';
 
 export class CreateFoodDto {
+  @IsString()
   name: string;
-
+  @IsNumber()
   nombre_calories: number;
-
+  @IsNumber()
   lipides: number;
-
+  @IsNumber()
   glucides: number;
-
+  @IsNumber()
   proteines: number;
-
+  @IsBoolean()
   validate: boolean;
 }
